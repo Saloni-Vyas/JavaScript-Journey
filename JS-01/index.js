33b478a2c85c52871be1c986b16b491b38af08c4
Saloni-Vyas <128497242+Saloni-Vyas@users.noreply.github.com>
2025-01-05 16:58:54 +0000

    Let , var and const variable

---
+++ JS-01/index.js
@@ -0,0 +1,10 @@
+
+const accountId = 144553;
+let accountPassword = "string e.com";
+var accountPasswordID = "12345";
+accountCity = "Jaipur";
+
+console.log("constant variable - "+ accountId);
+console.log("let variable - "+ accountPassword);
+console.log("var variable - "+ accountPasswordID);
+console.log("variable without initial with the datatype - "+ accountCity);
