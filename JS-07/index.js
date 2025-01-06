6de7fcc806120c98f7134c50f86a940c08bab068
Saloni-Vyas <128497242+Saloni-Vyas@users.noreply.github.com>
2025-01-06 10:26:55 +0000

    Date in JavaScript

---
+++ JS-07/index.js
@@ -0,0 +1,14 @@
+// Dates
+let myDate = new Date ( )
+console. log(myDate. toString() );
+console. log(myDate.toDateString());
+console. log(myDate. toLocaleString());
+console. log (typeof myDate) ;
+
+let myCreatedDate = new Date (2023);
+
+let myCreateDate = new Date(2023, 0, 23, 5, 3)
+console. log (myCreateDate. toDateString ( ) ) ;
+
+let Date_Create = new Date("01-14-2023")
+console. log (Date_Create. toLocaleString( ) ) ;
